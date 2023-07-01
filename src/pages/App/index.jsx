f88d9615823d59d08e2fdac70fd060716e789701
dancreactive23 <dan.creactive23@gmail.com>
2023-06-30 20:26:38 -0500

    Routes created and linked with components

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,3 +1,4 @@
+import {useRoutes,BrowserRouter} from 'react-router-dom';
 import Home from '../Home';
 import MyAccount from '../MyAccount';
 import Myorders from '../MyOrders';
@@ -6,16 +7,25 @@ import MyOrder from '../MyOrder';
 import NotFound from '../NotFound';
 import './App.css'
 
+const AppRoutes = () =>{
+  let routes = useRoutes([
+    {path:'/',element:<Home/>},
+    {path:'/my-account',element:<MyAccount/>},
+    {path:'/my-orders',element:<Myorders/>},
+    {path:'/sign-in',element:<Signin/>},
+    {path:'/my-order',element:<MyOrder/>},
+    {path:'/*',element:<NotFound/>},
+  ])
+  return routes;
+}
+
 function App() {
 
   return (
     <>
-      <Home/>
-      <MyAccount/>
-      <Myorders/>
-      <Signin/>
-      <MyOrder/>
-      <NotFound/>
+      <BrowserRouter>
+        <AppRoutes/>
+      </BrowserRouter>
     </>
   )
 }
